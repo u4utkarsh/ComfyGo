62edeee6749a9e410550406fcba00aa6cc57aa28
Utkarsh <u4utkarshraj@gmail.com>
2025-07-25 14:33:37 +0530

    completed user home page having loaction panel and vehicle panel fully working with animations

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
   // sample array for locations
   const locations = [
     "Hostel No-1, GNDEC Ludhiana, Gill Park",
@@ -12,9 +12,16 @@ const LocationSearchPanel = () => {
   return (
     <div>
       {/* this is just sample data */}
-      {locations.map((location) => {
+      {locations.map((location, index) => {
         return (
-          <div className="flex gap-2 border-2 p-3 border-gray-50 active:border-black rounded items-center justify-start my-2">
+          <div
+            onClick={() => {
+              props.setvehiclePanelOpen(true);
+              props.setPanelOpen(false);
+            }}
+            key={index}
+            className="flex gap-2 border-2 p-3 border-gray-50 active:border-black rounded items-center justify-start my-2"
+          >
             <h2 className="bg-[#eee] w-8 h-8 rounded-full flex items-center justify-center ">
               <i className="ri-map-pin-line"></i>
             </h2>
